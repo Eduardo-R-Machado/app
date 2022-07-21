bb86fa09f204842f950d1e7f875c1bae1db31c97
eduardo329 <eduardo_rj_18@hotmail.com>
2022-07-21 18:00:15 -0300

    Primeiro Commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Ola</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+header{
+  background-color: #2c3e50;
+  color: #fff;
+  padding: 20px;
+}
+</style>
