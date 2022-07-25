38be24ea6e6cc7af2d129938e5c42a5c275b2390
eduardo329 <eduardo_rj_18@hotmail.com>
2022-07-25 17:23:44 -0300

    Aula 06 Tempo 03:10

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <TheHeader />
+  <TheHeader v-show="showHeader" />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <h2 v-show="showFirstName">
+      Nome do usuário: {{ user.name }}
+    </h2>
+    <h2 v-if="showYear < 19">
+      idade do usuário: {{ user.years }}
+    </h2>
+  </div>
+
+  <section class="type">
+    <h3 v-if="typeUser === 'admin'"> Admin</h3>
+    <h3 v-else-if="typeUser === 'user'"> User</h3>
+    <h3 v-else-if="typeUser === 'Developer'">Desenvolvedor</h3>
+    <h3 v-else>Não definido</h3>
+  </section>
+
+
+  <section class="todos">
+    <div v-bind:key="obj.id" v-for="(obj, index) in todos">
+      {{ index }} - {{ obj.text }} - {{ obj.done }}
+
+    </div>
+  </section>
+
+
 </template>
+
+
+
+
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -13,11 +45,46 @@ export default {
   components: {
     HelloWorld,
     TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      showFirstName: true,
+      showYear: false,
+      typeUser: 'nothing',
+      user: {
+        name: 'John Doe',
+        years: 10
+      },
+      todos: [
+        {
+          id: 1,
+          text: 'Learn Vue.js',
+          done: true
+        },
+        {
+          id: 2,
+          text: 'Learn Vuex',
+          done: false
+        },
+        {
+          id: 3,
+          text: 'Learn Vue Router',
+          done: false
+        }
+      ]
+    }
   }
 }
 </script>
 
+
+
+
 <style>
+body{
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +92,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 0 auto;
+  padding: 0;
+}
+
+.todos{
+  margin: 0 auto;
+  width: 100%;
+  padding: 10px;
+  background: #f1f1f1;
+  text-align: left;
 }
 </style>
