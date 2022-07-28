30a963c4edf587f5bc25494f1bc9dd2f35ee4860
eduardo329 <eduardo_rj_18@hotmail.com>
2022-07-28 17:35:36 -0300

    Aula 08 começo

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,15 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 
   <div>
-    <h2 v-show="showFirstName">
+
+    <h1 :class="{'title': true, 'title-home': isHome }">
+       Curso Vue 3
+    </h1>
+
+    <h2 :class="nameUser" v-show="showFirstName">
       Nome do usuário: {{ user.name }}
     </h2>
-    <h2 v-if="showYear < 19">
+    <h2 :class="['online', {title: true, 'title-home': isHome}]" v-if="showYear < 19">
       idade do usuário: {{ user.years }}
     </h2>
   </div>
@@ -18,20 +23,17 @@
     <h3 v-else>Não definido</h3>
   </section>
 
-
   <section class="todos">
-    <div v-bind:key="obj.id" v-for="(obj, index) in todos">
+    <div :key="obj.id" v-for="(obj, index) in todos">
       {{ index }} - {{ obj.text }} - {{ obj.done }}
+
+      <img v-if="obj.imgSrc" id="user-avatar" :src="obj.imgSrc">
 
     </div>
   </section>
 
 
 </template>
-
-
-
-
 
 
 
@@ -48,29 +50,41 @@ export default {
   },
   data() {
     return {
+      isHome  : true,
+      nameUser: 'online',
+      classTitle: 'title',
       showHeader: true,
       showFirstName: true,
       showYear: false,
       typeUser: 'nothing',
+
       user: {
         name: 'John Doe',
-        years: 10
+        years: 10,
+        imgSrc: 'https://www.w3schools.com/howto/img_avatar.png',
       },
       todos: [
         {
           id: 1,
           text: 'Learn Vue.js',
-          done: true
+          done: true,
+          imgSrc: 'https://www.w3schools.com/howto/img_avatar.png',
         },
         {
           id: 2,
           text: 'Learn Vuex',
-          done: false
+          done: false,
+          imgSrc: 'https://www.w3schools.com/howto/img_avatar.png',
         },
         {
           id: 3,
           text: 'Learn Vue Router',
           done: false
+        },
+        {
+          id: 4,
+          text: 'Learn Vuex',
+          done: true,
         }
       ]
     }
@@ -96,11 +110,36 @@ body{
   padding: 0;
 }
 
+#user-avatar{
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+}
+
 .todos{
   margin: 0 auto;
   width: 100%;
   padding: 10px;
   background: #f1f1f1;
   text-align: left;
+}
+
+.title{
+  font-size: 2rem;
+  font-weight: bold;
+  color: #42b983;
+}
+
+.title-home{
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: red;
+  margin-top: 0;
+}
+
+.online{
+  text-transform: uppercase;
+  font-size: 1.5rem;
+  text-shadow: #2c3e50 1px 1px 1px;
 }
 </style>
